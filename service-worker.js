b08a56e1b687eba1e1b264c4ad3646df31e10653
strikertanmay <32292953+strikertanmay@users.noreply.github.com>
2020-04-13 18:05:42 +0000

    Deploying to gh-pages from  @ 584ec6fecb9cf27d6e79c37dbc0f9967963fbf36 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9b975224006e76b6b3e28ed6e2e33ee5.js"
+  "/precache-manifest.6c83b4ab0fdcfd2facbbeed46c89e579.js"
 );
 
 self.addEventListener('message', (event) => {
